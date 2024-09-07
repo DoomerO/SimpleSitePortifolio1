569b3bc20305a9af218d365e15845e7ccdd17279
João Victor Almeida <109368541+DoomerO@users.noreply.github.com>
2024-09-07 20:08:00 -0300

    build1

---
+++ src/script.js
@@ -0,0 +1,31 @@
+const headerBottomText = document.getElementById("headerBottomText")
+const linkWhatsapp1 = document.getElementsByClassName("linkWhatsapp1");
+const sectionLeftTitle = document.getElementsByClassName("sectionLeftTitle");
+const sectionRightTitle = document.getElementsByClassName("sectionRightTitle");
+const sectionLeftMsg = document.getElementsByClassName("sectionLeftMsg");
+const sectionRightMsg = document.getElementsByClassName("sectionRightMsg");
+const linkFooter = document.getElementsByClassName("linkFooter");
+const usefullLinksTitle = document.getElementById("usefullLinksTitle");
+
+headerBottomText.textContent = "Para entrar em contato";
+
+//link wahtsapp
+linkWhatsapp1[0].textContent = "clique aqui!";
+
+//section titles left
+sectionLeftTitle[0].textContent = "Transporte Rápido e Seguro";
+sectionLeftTitle[1].textContent = "Como Entrar em Contato?";
+
+//section titles right
+sectionRightTitle[0].textContent = "Onde nos Encontrar?";
+
+//section msgs left
+sectionLeftMsg[0].textContent = "Trabalhamos com fretes e mudanças, levando agílidade como um princípio do nosso serviço. Garantindo o cuidado de cada item transportado!!";
+sectionLeftMsg[1].textContent = "Combinamos todos os por menores de cada serviço por whatsapp. Sendo necessário entrar em contato pelo seguinte número: (24) 98110-0436.\nOu você pode apertar o botão no cabeçalho da página que irá te redirecionar ao whatsapp!";
+
+//section msgs right
+sectionRightMsg[0].textContent = "Prestamos serviços por  Volta Redonda , Barra Mansa, Pinheral e as demais cidades da região. Caso haja alguma dúvida sobre relativa a quais lugares atendêmos, entre em contato.";
+
+usefullLinksTitle.textContent = "Links Úteis:"
+
+linkFooter[0].textContent = "Whatsapp Web"
